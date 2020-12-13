6ff5d97ebe39f3d5c1d2b1a18b0c4af4906a7441
harshdeep.singh <Harshdeep.singh@pasarpolis.com>
2020-12-13 22:26:31 +0530

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -12,6 +12,6 @@ class Site:
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
-        for path in self.source.glob("*"):
-            if path.is_dir:
-                self.create_dir(path=path)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
