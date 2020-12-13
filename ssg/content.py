f8166b2c1ad7e1a7e9fad1dd6865d0f04b218e35
harshdeep.singh <Harshdeep.singh@pasarpolis.com>
2020-12-14 00:31:54 +0530

    Completed module

--- ssg/content.py
+++ ssg/content.py
@@ -4,9 +4,10 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
+    @classmethod
     def load(cls, string: str):
         _, fm, content = string.split(cls.__regex, maxsplit=2)
         metadata = load(fm, Loader=FullLoader)
